a54da1fc5a119ddf7f7941ddd06110f81dfc359c
Ashish Mishra <asismisr@CTIPL-MAC-348.local>
2025-03-21 21:58:48 +0530

    adding program for kafka/p04_singleProducerConsumer

---
+++ src/main/java/kafka/constantsClass/Constants.java
@@ -0,0 +1,15 @@
+package kafka.constantsClass;
+
+public final class Constants {
+
+    private Constants(){}
+
+    public static final String BOOTSTRAP_SERVER = "bootstrap.servers";
+    public static final String LOCALHOST_29092 = "localhost:29092";
+    public static final String STRING_SERIALIZER = "org.apache.kafka.common.serialization.StringSerializer";
+    public static final String KEY_SERIALIZER = "key.serializer";
+    public static final String VALUE_SERIALIZER = "value.serializer";
+    public static final String STRING_DESERIALIZER = "org.apache.kafka.common.serialization.StringDeserializer";
+    public static final String KEY_DESERIALIZER = "key.deserializer";
+    public static final String VALUE_DESERIALIZER = "value.deserializer";
+}
