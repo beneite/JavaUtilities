eec556e12aeeefb2bced0221defceb470e55e848
Ashish Mishra <asismisr@CTIPL-MAC-348.local>
2025-06-02 20:27:09 +0530

    adding 02_containerOnCustomNetwork

--- src/main/java/CI_CD/jenkins/p01_simpleJenkinsFile_pipeline/SimpleJavaProgram.java
+++ src/main/java/CI_CD/jenkins/p01_simpleJenkinsFile_pipeline/SimpleJavaProgram.java
@@ -1,5 +1,8 @@
 package CI_CD.jenkins.p01_simpleJenkinsFile_pipeline;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class SimpleJavaProgram {
 
     public static void main(String[] args){
